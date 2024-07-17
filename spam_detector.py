f6d8200acbd3e744a999532193663d2f9dd6ce3f
Taz Larson <tazlarson1@gmail.com>
2024-07-16 19:52:20 -0700

    train model

--- spam_detector.py
+++ spam_detector.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-import sklearn
-# print(sklearn.__version__)
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
+from sklearn.naive_bayes import MultinomialNB
 def load_data():
     url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
     data = pd.read_csv(url, sep='\t', header=None, names=['label','text' ])
@@ -13,6 +14,15 @@ def load_data():
 def train_model(data):
     x = data['text']
     y = data['label']
+    vectorizer = TfidfVectorizer()
+    x = vectorizer.fit_transform(x)
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2) #CHECK OUT WHAT THIS IS
+    model = MultinomialNB() #CHECK OUT WHAT THIS IS
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    return accuracy, report
 
 
 
@@ -20,4 +30,6 @@ def train_model(data):
 
 if __name__ == "__main__":
     data = load_data()
-    print(data)
+    accuracy, report = train_model(data)
+    print(f"Accuracy: \n{accuracy}")
+    print(f"Classification Report: \n{report}")
